680542854e64d936d6f53c71b50fe6c71e233614
Sahilkr0225 <sahiljsr05@gmail.com>
2025-04-13 11:57:57 +0530

    Add files via upload

---
+++ MovieCollectionOrganiserApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sahilkr0225.moviecollectionorganiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieCollectionOrganiserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
